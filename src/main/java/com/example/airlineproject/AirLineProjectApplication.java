458ecf2fc2dccba4ea6d3213bbd788a392d9eedb
Ardo777 <ardoavagyan7@gmail.com>
2024-03-04 16:17:17 +0400

    AirLIne Project

---
+++ src/main/java/com/example/airlineproject/AirLineProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.airlineproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirLineProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirLineProjectApplication.class, args);
+    }
+
+}
