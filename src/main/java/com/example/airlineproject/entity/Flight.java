191549a4b04393b033ba549a8e69d620d64fac50
Vahagn Sahakyan <vahagnsahakyan53@gmail.com>
2024-03-12 13:44:38 +0400

    admin/manager template already added

--- src/main/java/com/example/airlineproject/entity/Flight.java
+++ src/main/java/com/example/airlineproject/entity/Flight.java
@@ -1,5 +1,6 @@
 package com.example.airlineproject.entity;
 
+import com.example.airlineproject.entity.enums.Status;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
